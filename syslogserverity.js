4bc6e9bedda4bc3407ba535dbc71c212d7e50de1
llzxo <314040307@qq.com>
2016-03-12 22:14:50 +0800

    upload

---
+++ syslogserverity.js
@@ -0,0 +1,21 @@
+var MongoClient=require('mongodb').MongoClient;
+var assert=require('assert');
+var ObjectId=require('mongodb').ObjectID;
+var url='mongodb://localhost:27017/dbsyslog';
+var aggregatesyslog=function(db,callback){
+    db.collection('syslog').aggregate(
+        [
+            {$group:{"_id":"$serverity","count":{$sum:1}}}
+        ]
+    ).toArray(function(err,result){
+        assert.equal(err,null);
+        console.log(result);
+        callback(result);
+    });
+};
+MongoClient.connect(url,function(err,db){
+    assert.equal(null,err);
+    aggregatesyslog(db,function(){
+    db.close();
+    });
+});
